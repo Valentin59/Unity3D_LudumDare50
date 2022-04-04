95780ae98eca8b20b9d6d36a203310dcd9c3001c
Valentin <valenttaf@gmail.com>
2022-04-04 04:07:28 +0200

    LastCommitBugg Fixed

--- Assets/LoadSceneWinLose.cs
+++ Assets/LoadSceneWinLose.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSceneWinLose : MonoBehaviour
 {
+    public string winSceneName = "";
+    public string loseSceneName = "";
+
+    public Health player;
+    public Characters ennemiesInMap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player.onDieCallback.AddListener(LoadLoseScene);
+        ennemiesInMap.onEmptySetCallback.AddListener(LoadWinScene);
+    }
+
+    public void LoadLoseScene()
+    {
+        SceneManager.LoadScene(loseSceneName);
+    }
+
+    public void LoadWinScene()
+    {
+        SceneManager.LoadScene(winSceneName);
     }
 
     // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        player.onDieCallback.RemoveListener(LoadLoseScene);
+        ennemiesInMap.onEmptySetCallback.RemoveListener(LoadWinScene);
     }
 }
---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public float timeToSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
